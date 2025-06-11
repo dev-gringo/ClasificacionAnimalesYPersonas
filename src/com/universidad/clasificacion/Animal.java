c1c2b7f04e7bd14b6419bbdbbe60e4c2fef1d80a
stiven arias <steven.jhoan12365@gmail.com>
2025-06-10 19:27:26 -0500

    atributos privados por encapsulamiento

--- src/com/universidad/clasificacion/Animal.java
+++ src/com/universidad/clasificacion/Animal.java
@@ -1,6 +1,7 @@
 package com.universidad.clasificacion;
 
 public class Animal {
+    // Atributos
     private String nombre;
     private String tipo;   // terrestre, aéreo, acuático
     private String genero; // masculino, femenino
